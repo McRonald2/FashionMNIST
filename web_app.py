908a4a826c9651f1e296ead1b3539b8021090025
Ron <47794794+McRonald2@users.noreply.github.com>
2020-05-02 18:09:43 +0300

    Add files via upload

--- web_app.py
+++ web_app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template
 from inference import get_class_name
+import webbrowser
+from threading import Timer
 
 app = Flask(__name__)
 
@@ -18,5 +20,10 @@ def cnn_classifier():
         return render_template('result.html', class_idx=pred_class, class_name=cat_name)
 
 
+def open_browser():
+    webbrowser.open_new('http://127.0.0.1:5000/')
+
+
 if __name__ == '__main__':
+    Timer(1, open_browser).start()
     app.run(debug=False)
